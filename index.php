ffaf7fb338e098b0603645dd7a12c4534fd844c2
Mihail Pogonin <p-ma95@mail.ru>
2019-02-03 12:11:46 +0300

    Comment

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php
-
-    echo "Привет, git!";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/styles/style.css" rel="stylesheet">
+    <title>Главная страница</title>
+</head>
+<body>
+    <script src="/js/javascript.js"></script>
+</body>
+</html>
