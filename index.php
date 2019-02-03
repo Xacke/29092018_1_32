2aa1725186e5b57e9b4fc7be856d3b8874ece004
Mihail Pogonin <p-ma95@mail.ru>
2019-02-03 12:53:37 +0300

    изменен заголовок

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +8,7 @@
     <title>Главная страница</title>
 </head>
 <body>
-    <h1>Это наша супер страница 1</h1>
+    <h1>Это наша супер страница 2</h1>
     <script src="/js/javascript.js"></script>
 </body>
 </html>
